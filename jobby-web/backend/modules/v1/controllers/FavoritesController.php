29799c5b5cf74589198de5f531583ec92b49ccbe
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-05 15:43:58 +0000

    Backoffice - Favorites Controller (API - Module v1)

--- jobby-web/backend/modules/v1/controllers/FavoritesController.php
+++ jobby-web/backend/modules/v1/controllers/FavoritesController.php
@@ -2,11 +2,41 @@
 
 namespace backend\modules\v1\controllers;
 
-class FavoritesController extends \yii\web\Controller
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+
+class FavoritesController extends ActiveController
 {
-    public function actionIndex()
+    public $modelClass = 'frontend\models\Favorite';
+
+    public function behaviors()
     {
-        return $this->render('index');
+        $behaviors = parent::behaviors(); $behaviors['authenticator'] = [
+        'class' => QueryParamAuth::className(),
+    ];
+        return $behaviors;
     }
 
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($action === 'post' or $action === 'delete'){
+            if (\Yii::$app->user->isGuest) {
+                throw new \yii\web\ForbiddenHttpException('Apenas poderá ' . $action . ' utilizadores registados...');
+            }
+        }
+    }
+
+    public function actionGetFavoriteFromUserId(){
+        $user_id = \Yii::$app->request->post('user_id');
+
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $result = $this->modelClass::find()->where(['user_id' => $user_id])->all();
+
+        if(isset($result)){
+            return $result;
+        }
+        else{
+            throw new \yii\web\NotFoundHttpException;
+        }
+    }
 }
