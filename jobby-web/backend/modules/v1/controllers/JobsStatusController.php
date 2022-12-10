32fa2dd8699b519814cb7540596d7d42f0e71665
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-10 17:31:02 +0000

    Backoffice - JobsStatus Controller (API - Module v1)

--- jobby-web/backend/modules/v1/controllers/JobsStatusController.php
+++ jobby-web/backend/modules/v1/controllers/JobsStatusController.php
@@ -2,11 +2,27 @@
 
 namespace backend\modules\v1\controllers;
 
-class JobsStatusController extends \yii\web\Controller
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+
+class JobsStatusController extends ActiveController
 {
-    public function actionIndex()
+    public $modelClass = 'common\models\JobStatus';
+
+    public function behaviors()
     {
-        return $this->render('index');
+        $behaviors = parent::behaviors(); $behaviors['authenticator'] = [
+        'class' => QueryParamAuth::className(),
+    ];
+        return $behaviors;
     }
 
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($action === 'post' or $action === 'delete'){
+            if (\Yii::$app->user->isGuest) {
+                throw new \yii\web\ForbiddenHttpException('Apenas poderá ' . $action . ' utilizadores registados...');
+            }
+        }
+    }
 }
