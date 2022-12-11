4abd2dc25ebb60d09e1bf85deb25f721f55e4b60
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-11 21:13:03 +0000

    Backoffice - Plans Controller (API - Module v1)

--- jobby-web/backend/modules/v1/controllers/PlansController.php
+++ jobby-web/backend/modules/v1/controllers/PlansController.php
@@ -2,11 +2,27 @@
 
 namespace backend\modules\v1\controllers;
 
-class PlansController extends \yii\web\Controller
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+
+class PlansController extends ActiveController
 {
-    public function actionIndex()
+    public $modelClass = 'common\models\Plan';
+
+    public function behaviors()
     {
-        return $this->render('index');
+        $behaviors = parent::behaviors(); $behaviors['authenticator'] = [
+        'class' => QueryParamAuth::className(),
+    ];
+        return $behaviors;
     }
 
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($action === 'post' or $action === 'delete'){
+            if (\Yii::$app->user->isGuest) {
+                throw new \yii\web\ForbiddenHttpException('Apenas poderá ' . $action . ' utilizadores registados...');
+            }
+        }
+    }
 }
