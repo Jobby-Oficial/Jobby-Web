63ebc8dacadc459a409a9fa1354db00961fac96f
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-15 10:52:44 +0000

    Backoffice - Schedules Controller (API - Module v1)

--- jobby-web/backend/modules/v1/controllers/SchedulesController.php
+++ jobby-web/backend/modules/v1/controllers/SchedulesController.php
@@ -2,11 +2,34 @@
 
 namespace backend\modules\v1\controllers;
 
-class SchedulesController extends \yii\web\Controller
+use common\models\Schedule;
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+
+class SchedulesController extends ActiveController
 {
-    public function actionIndex()
+    public $modelClass = 'common\models\Schedule';
+
+    public function behaviors()
     {
-        return $this->render('index');
+        $behaviors = parent::behaviors(); $behaviors['authenticator'] = [
+        'class' => QueryParamAuth::className(),
+    ];
+        return $behaviors;
     }
 
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($action === 'post' or $action === 'delete'){
+            if (\Yii::$app->user->isGuest) {
+                throw new \yii\web\ForbiddenHttpException('Apenas poderá ' . $action . ' utilizadores registados...');
+            }
+        }
+    }
+
+    public function actionGetSchedulesFromClientId(){
+        $schedules = Schedule::find()->where(['client_id' => \Yii::$app->request->post('client_id')])->all();
+
+        return $schedules;
+    }
 }
