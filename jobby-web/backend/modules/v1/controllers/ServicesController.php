b30bf3174573e6f6e9c48474cb32e1d10da11fb7
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-18 20:30:45 +0000

    Backoffice - Services Controller (API - Module v1)

--- jobby-web/backend/modules/v1/controllers/ServicesController.php
+++ jobby-web/backend/modules/v1/controllers/ServicesController.php
@@ -2,11 +2,49 @@
 
 namespace backend\modules\v1\controllers;
 
-class ServicesController extends \yii\web\Controller
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+
+class ServicesController extends ActiveController
 {
-    public function actionIndex()
+    public $modelClass = 'common\models\Service';
+
+    public function behaviors()
     {
-        return $this->render('index');
+        $behaviors = parent::behaviors(); $behaviors['authenticator'] = [
+        'class' => QueryParamAuth::className(),
+    ];
+        return $behaviors;
     }
 
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($action === 'post' or $action === 'delete'){
+            if (\Yii::$app->user->isGuest) {
+                throw new \yii\web\ForbiddenHttpException('Apenas poderá ' . $action . ' utilizadores registados...');
+            }
+        }
+    }
+
+    /* public function fields()
+    {
+        return [
+            'user_id' => function ($model) {
+                return $model->user->username; // Return related model property, correct according to your structure
+            },
+        ];
+    } */
+
+    public function actionGetServiceFromUserId(){
+        $user_id = \Yii::$app->request->post('user_id');
+
+        $result = $this->modelClass::find()->where(['user_id' => $user_id])->all();
+
+        if(isset($result)){
+            return ['response' => $result];
+        }
+        else{
+            throw new \yii\web\NotFoundHttpException;
+        }
+    }
 }
