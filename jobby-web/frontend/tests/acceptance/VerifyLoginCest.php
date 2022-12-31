609d8973e40f7c6daa9759d9650fff51ce41616e
GuilhermeCruzPT <guilhermecruz128@gmail.com>
2022-12-31 02:03:42 +0000

    Codeception - VerifyLoginCest (Acceptance)

---
+++ jobby-web/frontend/tests/acceptance/VerifyLoginCest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace frontend\tests\Acceptance;
+
+use frontend\tests\AcceptanceTester;
+
+class VerifyLoginCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('site/login');
+        $I->seeInCurrentUrl('site/login');
+    }
+
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Verify Login');
+        $I->fillField('LoginForm[username]', 'teste');
+        $I->fillField('LoginForm[password]', 'teste1234');
+        $I->seeInField('LoginForm[username]', 'teste');
+        $I->seeInField('LoginForm[password]', 'teste1234');
+        $I->seeLink('Entrar');
+        $I->click('login-button');
+        $I->wait(2);
+        $I->seeInCurrentUrl('/');
+        $I->seeElement('.nav');
+        $I->see('teste', '.nav');
+    }
+}
